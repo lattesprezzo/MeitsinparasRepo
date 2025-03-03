1e5c2a0d112fca42462551d1574872d421af3479
Sandvich <dfvs314@protonmail.com>
2025-03-03 12:42:29 +0200

    movement script update1

--- MovementScript.cs
+++ MovementScript.cs
@@ -1,1 +1,15 @@
-using unity
+using UnityEngine;
+
+public class MovementScript : MonoBehavior
+{
+    float speed = 5f;
+
+    void Update()
+    {
+        move();
+    }
+
+    void move(){
+        float hInput
+    }
+}
