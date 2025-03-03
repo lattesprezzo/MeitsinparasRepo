ca39d120fac9a699558d72724d573b3c5e033655
Lattesprezzo <155761412+lattesprezzo@users.noreply.github.com>
2025-03-03 14:14:00 +0200

    Update House.cs

--- Assets/House.cs
+++ Assets/House.cs
@@ -5,7 +5,7 @@ public class House : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //URpaPESPaskegajsghjoasrg
     }
 
     // Update is called once per frame
